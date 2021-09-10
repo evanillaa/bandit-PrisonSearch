00873aef2e6dfe72165681bc674d4161d36fee72
FreakOutModz <75781403+FreakOutModz@users.noreply.github.com>
2021-09-10 11:39:34 -0400

    remove(esx): Transition to esx-legacy

--- client/bandit-client.lua
+++ client/bandit-client.lua
@@ -1,12 +1,3 @@
-ESX = nil
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(5)
-	end
-end)
-
-
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(1)
