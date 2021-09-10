0a75184a3dbbefefba498be6a7467e7918987ee6
FreakOutModz <75781403+FreakOutModz@users.noreply.github.com>
2021-09-10 11:55:30 -0400

    feat(config): Utilisation des vector3

--- config.lua
+++ config.lua
@@ -4,21 +4,19 @@ Config = {}
 Config.banditSearch = {
 
     searchPositions = {
-        {x=1763.23, y=2570.81, z=50.55},
-        {x=1769.05, y=2578.62, z=45.8},
-        {x=1766.14, y=2580.2, z=50.55},
-        {x=1761.03, y=2580.5, z=45.8},
-        {x=1778.13, y=2617.87, z=50.55},
-        {x=1770.92, y=2616.09, z=50.55},
-        {x=1689.46, y=2551.74, z=45.46},
-        {x=1631.58, y=2527.56, z=45.46},
-        {x=1778.52, y=2555.2, z=45.8},
-        {x=1776.5, y=2599.38, z=45.8}
+        {vector3(1763.23, 2570.81, 50.55)},
+        {vector3(1769.05, 2578.62, 45.8)},
+        {vector3(1766.14, 2580.2, 50.55)},
+        {vector3(1761.03, 2580.5, 45.8)},
+        {vector3(1778.13, 2617.87, 50.55)},
+        {vector3(1770.92, 2616.09, 50.55)},
+        {vector3(1689.46, 2551.74, 45.46)},
+        {vector3(1631.58, 2527.56, 45.46)},
+        {vector3(1778.52, 2555.2, 45.8)},
+        {vector3(1776.5, 2599.38, 45.8)},
     },
 
-    -- Tu peux mettre tes items customs ici -->
-
-    giveRandomItems = {
+    giveRandomItems = { -- Tu peux mettre tes items customs ici
            "bread",
            "weed"
     },
@@ -33,10 +31,6 @@ Config.banditSearch = {
     progressBarTimer = 20000,
 
     progressBarTimerText = "Vous cherchez des items...",
-
-    Animation = 'PROP_HUMAN_BUM_BIN',
-
-    sleepThread = 1000
 }
 
 
