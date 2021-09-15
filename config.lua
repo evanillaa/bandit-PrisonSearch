8949afd74c47bbf017104f6e5eca6e5272487cbd
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-09-15 23:51:28 +0300

    I will try it

--- config.lua
+++ config.lua
@@ -1,38 +1,63 @@
 Config = {}
 
-
 Config.banditSearch = {
 
-    searchPositions = {
-        {vector3(1763.23, 2570.81, 50.55)},
-        {vector3(1769.05, 2578.62, 45.8)},
-        {vector3(1766.14, 2580.2, 50.55)},
-        {vector3(1761.03, 2580.5, 45.8)},
-        {vector3(1778.13, 2617.87, 50.55)},
-        {vector3(1770.92, 2616.09, 50.55)},
-        {vector3(1689.46, 2551.74, 45.46)},
-        {vector3(1631.58, 2527.56, 45.46)},
-        {vector3(1778.52, 2555.2, 45.8)},
-        {vector3(1776.5, 2599.38, 45.8)},
+    ["searchPositions"] = {
+             [1] = {
+                 ["coords"] = vector3(1763.23, 2570.81, 50.55),
+                  ["isOpened"] = false
+             },
+            [2] = {
+               ["coords"] = vector3(1769.05, 2578.62, 45.8),
+                  ["isOpened"] = false
+            },
+           [3] = {
+              ["coords"] = vector3(1766.14, 2580.2, 50.55),
+                  ["isOpened"] = false
+            },
+            [4] = {
+               ["coords"] = vector3(1761.03, 2580.5, 45.8),
+               ["isOpened"] = false
+            },
+             [5] = {
+                ["coords"] = vector3(1778.13, 2617.87, 50.55),
+                ["isOpened"] = false
+             },
+             [6] = {
+                ["coords"] = vector3(1770.92, 2616.09, 50.55),
+                  ["isOpened"] = false
+            },
+            [7] = {
+                ["coords"] = vector3(1689.46, 2551.74, 45.46),
+                     ["isOpened"] = false
+             },
+             [8] = {
+                 ["coords"] = vector3(1631.58, 2527.56, 45.46),
+                     ["isOpened"] = false
+            },
+             [9] = {
+                 ["coords"] = vector3(1778.52, 2555.2, 45.8),
+                    ["isOpened"] = false
+             },
+             [10] = {
+                 ["coords"] = vector3(1776.5, 2599.38, 45.8),
+                     ["isOpened"] = false
+             },
     },
 
-    giveRandomItems = { -- Tu peux mettre tes items customs ici
+    ["giveRandomItems"] = { -- Tu peux mettre tes items customs ici
            "bread",
            "weed"
     },
 
-    Chance = {
+    ["Chance"] = {
         mininum = 2,
         maximum = 4
     },
 
-    drawText = "[~g~E~s~] pour chercher...",
+    ["drawText"] = "[~g~E~s~] pour chercher...",
 
-    progressBarTimer = 20000,
+    ["progressBarTimer"] = 20000,
 
-    progressBarTimerText = "Vous cherchez des items...",
+    ["progressBarTimerText"] = "Vous cherchez des items...",
 }
-
-
-
-
